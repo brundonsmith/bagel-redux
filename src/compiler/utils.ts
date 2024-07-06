8c0bda47e551fc090296bf090fd41649f66d2525
Brandon Smith <mail@brandons.me>
2024-07-06 16:24:15 -0500

    Expand profiling utility

--- src/compiler/utils.ts
+++ src/compiler/utils.ts
@@ -24,12 +24,49 @@ export function zip<A, B>(arr1: A[], arr2: B[], mode: 'truncate' | 'left' | 'rig
 	return res
 }
 
+let timings = new Map<string, number>()
+let counts = new Map<string, number>()
+let callStack: string[] = []
+const add = (map: Map<string, number>, name: string, time: number) => map.set(name, (map.get(name) ?? 0) + time)
+const pad = (str: string) => str + new Array(20 - str.length).fill(' ').join('')
+
+const profilingEnabled = false
+
 // eslint-disable-next-line @typescript-eslint/ban-types
-export const instrument = <F extends Function>(name: string, fn: F): F => {
+export const profile = <F extends Function>(name: string, fn: F): F => {
+	if (!profilingEnabled) return fn
+
 	return ((...args: any[]) => {
+		const isFirst = callStack.length === 0
+
+		callStack.push(name)
 		const start = Date.now()
 		const result = fn(...args)
-		console.log(`${name} took ${Date.now() - start}ms`)
+		const myRuntime = Date.now() - start
+		callStack.pop()
+
+		add(counts, name, 1)
+		add(timings, name, myRuntime) // add this runtime to this function's record
+
+		if (!isFirst) {
+			add(timings, callStack[callStack.length - 1]!, -1 * myRuntime) // subtract this runtime from parent functions' records
+		} else {
+			// done with this root call
+			const entries = Array.from(timings.entries()).sort(([_0, timeA], [_1, timeB]) => timeB - timeA)
+			const total = entries.reduce((acc, [_, time]) => acc + time, 0)
+
+			console.log(`${name} took ${total}ms`)
+			for (const [name, time] of entries) {
+				const count = counts.get(name)!
+				const averageTime = time / count
+				console.log(`  ${pad(name)} | ${(100 * time / total).toFixed(0)}% | ${count} | Avg ${averageTime}ms per call`)
+			}
+
+			timings = new Map()
+			counts = new Map()
+			callStack = []
+		}
+
 		return result
 	}) as unknown as F
 }
